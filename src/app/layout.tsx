ff15f9c556527d1be996e8bb4405264b08efdef0
rishicds <124495375+rishicds@users.noreply.github.com>
2024-06-27 00:40:26 +0530

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "../components/common/Navbar";
-import Footer from "@/components/common/Footer";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +21,6 @@ export default function RootLayout({
       <body>
         <Navbar />
         <main>{children}</main>
-        <Footer />
       </body>
     </html>
   );
