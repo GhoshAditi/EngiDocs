4291114c0a79bca0962db49cf59dbd7731f75571
rishicds <124495375+rishicds@users.noreply.github.com>
2024-06-27 22:18:49 +0530

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 "use client"
-
-import Card from "@/components/home/Card";
 import ShiftingContactForm from "@/components/home/ContactUs";
 import Testimonial from "@/components/home/Testimonials";
 import { Content } from "@/components/home/summarizer";
@@ -11,7 +9,6 @@ export default function Home() {
   return (
     <>
      <Content/>
-     <Card/>
      <Testimonial/>
      <ShiftingContactForm/>
     </>
