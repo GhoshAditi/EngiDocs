4d092f2ffb741cb6bc375d207973500d10552062
rishicds <124495375+rishicds@users.noreply.github.com>
2024-06-27 18:38:54 +0530

    dd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
 "use client"
-import Image from "next/image";
-import {Content}  from '../components/home/summarizer';
-import Card from "../components/home/Card";
-import ShiftingContactForm from "../components/home/ContactUs";
-import Testimonial from "../components/home/Testimonial";
+
+import Card from "@/components/home/Card";
+import ShiftingContactForm from "@/components/home/ContactUs";
+import Testimonial from "@/components/home/Testimonials";
+import { Content } from "@/components/home/summarizer";
+
 
 
 export default function Home() {
   return (
     <>
-      <Content />
-      <Card />
-      <Testimonial />
-      <ShiftingContactForm />
+     <Content/>
+     <Card/>
+     <ShiftingContactForm/>
+     <Testimonial/>
     </>
   );
 }
