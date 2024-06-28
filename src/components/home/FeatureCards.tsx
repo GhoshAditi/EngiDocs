c519f45f76256042314ef872feb0f417b1202abb
GhoshAditi <hena.ghosh786@gmail.com>
2024-06-28 12:25:31 +0530

    card added

--- src/components/home/FeatureCards.tsx
+++ src/components/home/FeatureCards.tsx
@@ -72,25 +72,6 @@ export const CardContainer = ({
           {children}
         </div>
       </div>
-    </MouseEnterContext.Provider>
-  );
-};
-
-export const CardBody = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return (
-    <div
-      className={cn(
-        "h-96 w-96 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
-        className
-      )}
-    >
-      {children}
     </div>
   );
 };
